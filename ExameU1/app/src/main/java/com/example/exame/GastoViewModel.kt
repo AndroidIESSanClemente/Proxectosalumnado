283c9ddf86eb77899e9011a543f31835fd75202f
Laura Perez <lauraperezorosa@gmail.com>
2026-02-05 23:28:14 +0100

    add ex

--- ExameU1/app/src/main/java/com/example/exame/GastoViewModel.kt
+++ ExameU1/app/src/main/java/com/example/exame/GastoViewModel.kt
@@ -1,31 +1,4 @@
 package com.example.exame
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-//ViewModel que almacena o gasto total. Úsase LiveData para que os fragments observen e se actualicen automaticamente
-
-class GastoViewModel : ViewModel() {
-
-    // LiveData interno que almacena o total, só o pode modificar o ViewModel
-    private val _totalGasto = MutableLiveData(0.0)
-
-    // LiveData público para observar dende os fragments
-    val totalGasto: LiveData<Double> get() = _totalGasto
-
-    // Este LiveData poñerase en 'true' cando o gasto se garde
-    private val _gastoGardado = MutableLiveData<Boolean>()
-    val gastoGardado: LiveData<Boolean> get() = _gastoGardado
-
-    // Metodo que suma un gasto ao total
-    fun engadirGasto(cantidade: Double) {
-        _totalGasto.value = (_totalGasto.value ?: 0.0) + cantidade
-        _gastoGardado.value = true
-    }
-
-    // Metodo que resetea o gasto total a cero
-    fun limparGasto() {
-        _totalGasto.value = 0.0
-    }
+class GastoViewModel {
 }
--- ExameU1/app/src/main/java/com/example/exame/MainActivity.kt
+++ ExameU1/app/src/main/java/com/example/exame/MainActivity.kt
@@ -5,59 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.example.exame.databinding.ActivityMainBinding
-import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
-
-    //Variable para almacenar a referencia do View Binding
-    private lateinit var binding: ActivityMainBinding
-
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // Inflar o deseño utilizando View Binding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        // AppBarConfiguration con DrawerLayout
-        appBarConfiguration = AppBarConfiguration.Builder(navController.graph)
-            .setOpenableLayout(binding.drawerLayout)
-            .build()
-
-        // Conecta ActionBar (hamburguesa / atrás) con NavController
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        // NavigationView con NavController
-        binding.navView.setupWithNavController(navController)
-
-    }
-
-    //Permite que a frecha de ir cara atrás funcione
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
